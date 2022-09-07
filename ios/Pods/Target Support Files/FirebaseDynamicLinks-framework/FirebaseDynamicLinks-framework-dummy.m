a83927649e3aaf379041459c9c38e2372f7f7858
bitrequest <mail@bitrequest.io>
2022-09-07 13:47:34 +0200

    remove pods

--- ios/Pods/Target Support Files/FirebaseDynamicLinks-framework/FirebaseDynamicLinks-framework-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_FirebaseDynamicLinks_framework : NSObject
-@end
-@implementation PodsDummy_FirebaseDynamicLinks_framework
-@end
--- ios/Pods/Target Support Files/FirebaseDynamicLinks-library/FirebaseDynamicLinks-library-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_FirebaseDynamicLinks_library : NSObject
-@end
-@implementation PodsDummy_FirebaseDynamicLinks_library
-@end
